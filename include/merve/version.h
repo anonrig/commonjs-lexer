886cfbd9318463b937e7d58906c5a745b56e8a0b
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-01-29 20:59:20 +0100

    chore: add Release Please GHA workflow (#4)

--- include/merve/version.h
+++ include/merve/version.h
@@ -5,14 +5,14 @@
 #ifndef MERVE_VERSION_H
 #define MERVE_VERSION_H
 
-#define MERVE_VERSION "1.0.0"
+#define MERVE_VERSION "1.0.0" // x-release-please-version
 
 namespace lexer {
 
 enum {
-  MERVE_VERSION_MAJOR = 1,
-  MERVE_VERSION_MINOR = 0,
-  MERVE_VERSION_REVISION = 0,
+  MERVE_VERSION_MAJOR = 1,     // x-release-please-major
+  MERVE_VERSION_MINOR = 0,     // x-release-please-minor
+  MERVE_VERSION_REVISION = 0,  // x-release-please-patch
 };
 
 }  // namespace lexer
